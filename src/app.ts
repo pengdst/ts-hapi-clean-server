81db4f33af06e1b5fa13c1e56c7bf4795c656952
pengdst <pengkuh.dst@gmail.com>
2023-04-17 06:53:44 +0700

    use process.env to host

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import Server from './server';
 
-const server = new Server('localhost', process.env.NODE_PORT ?? 5000);
+const server = new Server(process.env.NODE_HOST ?? 'localhost', process.env.NODE_PORT ?? 5000);
 server.run();
